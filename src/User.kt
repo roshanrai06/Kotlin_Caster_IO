d1f283a975024c1cb2cffc73ae8df758faddd4d3
Roshan Rai <rrai@mast.com>
2020-07-11 20:18:47 +0530

    Init Block

--- src/User.kt
+++ src/User.kt
@@ -1,13 +1,40 @@
-class User(var firstName: String, var lastName: String) {
-    constructor(firstName: String, lastName: String, isPlatinum: Boolean)
+class User(private var firstName: String, private var lastName: String) {
+    var fullName: String = "$firstName $lastName"
+        get() = "First Full Name $field"
+        set(value) {
+            if (value.startsWith("Roshan")) {
+                field = "Roshan The Great"
+            }
+        }
+
+    constructor(firstName: String, lastName: String, isPlatinum: Boolean) : this(firstName, lastName) {
+        println("$firstName $lastName $isPlatinum")
+
+    }
+
+    constructor(firstName: String) : this(firstName, "") {
+        println(firstName)
+
+    }
 
     fun printFullName() {
         println("$firstName $lastName")
     }
 
+    companion object {
+        fun createUser(firstName: String, lastName: String): User {
+            return User(firstName, lastName)
+        }
+    }
+
 }
 
 fun main() {
-    val user = User("Roshan", "Rai")
-    user.printFullName()
+    val user = User("Roshan", "Rai", false)
+    val user1 = User("Roshan")
+    println(user1.fullName)
+    user1.fullName = "Roshan"
+    println(user1.fullName)
+    // user.printFullName()
+    User.createUser("Roshan", "Rai Companion").printFullName()
 }
