a77138eaa1c1cf25db77cf6845cd9fe3b32f31fe
Roshan Rai <rrai@mast.com>
2020-07-12 19:56:53 +0530

    Static Enum

--- src/Enum.kt
+++ src/Enum.kt
@@ -6,14 +6,23 @@ fun main(args: Array<String>) {
     val valueWithAbstract = AccountTypeWithAbstractFun.valueOf("gold".toUpperCase())
     println(valueWithAbstract)
     println(valueWithAbstract.calculateDiscountPercentage())
-
+    println("Enum List ******")
+    for (accountType in AccountTypeWithAbstractFun.values()) {
+        println(accountType)
+    }
+    println("getAccountTypeByName **")
+    println(AccountType.getAccountTypeByName("Bronze"))
 }
 
 enum class AccountType(val discountPercent: Int, val numberOfSubscription: Int) {
     BRONZE(10, 1),
     SILVER(15, 5),
     GOLD(20, 10),
-    PLATINUM(25, 20)
+    PLATINUM(25, 20);
+
+    companion object {
+        fun getAccountTypeByName(name: String) = valueOf(name.toUpperCase())
+    }
 }
 
 enum class AccountTypeWithAbstractFun() {
