f191ae4f384175d35c6f067c54360375df3eed0a
Roshan Rai <roshanrai06@gmail.com>
2020-07-19 17:04:36 +0530

    Test

--- src/Extension.kt
+++ src/Extension.kt
@@ -3,6 +3,7 @@ fun String.initials(): String {
     val firstInitial = values[0].substring(0, 1)
     val lastInitial = values[1].substring(0, 1)
     return "$firstInitial$lastInitial"
+
 }
 
 fun Int.isAdult(): Boolean = this >= 18
